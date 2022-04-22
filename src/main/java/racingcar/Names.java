213c30563db7caf354bbe22565850bec273a5b00
wenodev <logicalstark1105@gmail.com>
2022-04-22 12:25:22 +0900

    feat(NameUtils.java): string을 List로 변환합니다

--- src/main/java/racingcar/Names.java
+++ src/main/java/racingcar/Names.java
@@ -3,10 +3,16 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
+import static racingcar.NameUtils.mapToList;
+
 public class Names {
     private final List<Name> names;
 
     public Names(List<Name> names) {
         this.names = new ArrayList<>(names);
+    }
+
+    public Names(String strNames) {
+        this.names = mapToList(strNames);
     }
 }
