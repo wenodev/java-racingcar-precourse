b9662e24d135a527c67d0d212730aeb7b59cb810
wenodev <logicalstark1105@gmail.com>
2022-04-22 11:52:22 +0900

    feat(Cars.java): Cars는 여러개의 car로 가집니다.

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = new ArrayList<>(cars);
+    }
+}
