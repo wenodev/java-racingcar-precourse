cee37dc6f19ed27eaf2cf974c0f3eb011b6dde0e
wenodev <logicalstark1105@gmail.com>
2022-04-25 22:57:06 +0900

    feat(Game.java): 자동차 경주를 실행합니다.

--- src/main/java/racingcar/View/View.java
+++ src/main/java/racingcar/View/View.java
@@ -1,7 +1,15 @@
 package racingcar.View;
 
 public class View {
-    public static void out(String message){
+    public static void println(String message){
         System.out.println(message);
+    }
+
+    public static void print(String message){
+        System.out.print(message);
+    }
+
+    public static void printObj(Object object){
+        System.out.print(object);
     }
 }
